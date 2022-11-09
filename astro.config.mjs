babfcc326c9b690b9fe1296e1ff106797dbd2e89
Antony Faris <acp325@gmail.com>
2022-11-08 20:04:56 -0500

    Deploy to gh pages (#9)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://Mountain-Cabins-Utah.github.io",
+  base: "/website-v2",
+});
