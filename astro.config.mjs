5e02d3602cd43953f5b9d29ed1d6405937aeffa1
Antony Faris <acp325@gmail.com>
2022-11-08 20:11:47 -0500

    Revert gh pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://Mountain-Cabins-Utah.github.io",
-  base: "/website-v2",
-});
+export default defineConfig();
