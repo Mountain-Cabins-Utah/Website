026e6685eb1e2fadf86d795809cfe78f92856ba8
Antony Faris <acp325@gmail.com>
2022-11-09 19:06:38 -0500

    append extra data

--- src/scripts/generateJson.ts
+++ src/scripts/generateJson.ts
@@ -1,9 +1,24 @@
 import { getAllCabins } from "../api/cabins/getAllCabins";
+import { getCabinGallery } from "../api/cabins/getCabinGallery";
 import { writeFile } from "fs/promises";
+import extraData from "../data/extra-data.json";
 
 export async function generateJson() {
   const cabins = await getAllCabins({ cached: false });
-  await writeFile("./src/data/cabins.json", JSON.stringify(cabins));
+
+  const cabinsExtraData = await Promise.all(
+    cabins.map(async (cabin) => {
+      const gallery = await getCabinGallery(cabin.uid);
+      const extra = extraData.find((e) => e.uid === cabin.uid);
+      return {
+        ...cabin,
+        ...extra,
+        gallery,
+      };
+    })
+  );
+
+  await writeFile("./src/data/cabins.json", JSON.stringify(cabinsExtraData));
 }
 
 await generateJson();
