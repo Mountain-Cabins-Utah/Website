27f0886ec4a1321dfbabc55bcce8b6accfd9b3d9
Antony Faris <acp325@gmail.com>
2022-11-12 10:16:40 -0500

    Fix cabin details gallery

---
+++ src/components/cabinDetail/Gallery.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import "photoswipe/style.css";
+  import type { Picture } from "../../api/cabins/getCabin";
+
+  export let fullGallery: Picture[];
+  let galleryID = "gallery-ps";
+
+  let galleryId: HTMLDivElement;
+
+  let loadAllImages = false;
+
+  onMount(async () => {
+    // @ts-ignore
+    const PhotoSwipeLightbox = (await import("photoswipe/lightbox")).default;
+    let lightbox = new PhotoSwipeLightbox({
+      gallery: galleryId,
+      children: "a",
+      pswpModule: () => import("photoswipe"),
+    });
+    lightbox.init();
+    lightbox.on("beforeOpen", () => {
+      loadAllImages = true;
+    });
+    lightbox.on("close", () => {
+      loadAllImages = false;
+    });
+  });
+</script>
+
+<div class="gallery pswp-gallery" id={galleryID} bind:this={galleryId}>
+  {#each fullGallery as image, i}
+    {#if i < 6}
+      <a
+        href={image.url}
+        data-pswp-width={900}
+        data-pswp-height={900}
+        target="_blank"
+        rel="noreferrer"
+        class="item"
+      >
+        <img class="thumbnail" src={image.url} alt={image.description} />
+      </a>
+    {:else}
+      <a
+        href={image.url}
+        data-pswp-width={900}
+        data-pswp-height={900}
+        target="_blank"
+        rel="noreferrer"
+        class="item hidden-img"
+      >
+        {#if loadAllImages}
+          <img src={image.url} alt={image.description} />
+        {/if}
+      </a>
+    {/if}
+  {/each}
+</div>
+
+<style>
+  .gallery {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1rem;
+  }
+
+  .gallery .hidden-img {
+    display: none;
+  }
+
+  .thumbnail {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  /* RESPONSIVE */
+  @media only screen and (max-width: 768px) {
+    .gallery {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+</style>
