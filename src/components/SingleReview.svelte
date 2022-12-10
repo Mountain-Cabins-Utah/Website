396cffa43f43f348dccfd13588226bc782d6a946
Antony Faris <acp325@gmail.com>
2022-12-10 13:22:24 -0500

    multiple fixes

--- src/components/SingleReview.svelte
+++ src/components/SingleReview.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import { onMount } from "svelte";
   import reviews from "../data/reviews.json";
 
@@ -7,68 +7,37 @@
   // remove repeat author
   const filteredReviews = reviews.filter(
     (review, index, self) =>
-      index === self.findIndex((t) => t.author === review.author)
+      index ===
+      self.findIndex(
+        (t) => t.author === review.author && review.content.length < 270
+      )
   );
   const randomizedReviews = filteredReviews.sort(() => Math.random() - 0.5);
 
   let author = "";
   let content = "";
-  let reviewsIndex = 0;
-
-  // function generateReview() {
-  //   const randomReview = reviews[Math.floor(Math.random() * reviews.length)];
-  //   author = randomReview.author;
-  //   content = randomReview.content;
-  // }
 
   onMount(() => {
     author = randomizedReviews[0].author;
     content = randomizedReviews[0].content;
   });
-
-  function nextReview() {
-    reviewsIndex++;
-    const review = randomizedReviews[reviewsIndex];
-    author = review.author;
-    content = review.content;
-  }
-
-  function prevReview() {
-    reviewsIndex--;
-    const review = randomizedReviews[reviewsIndex];
-    author = review.author;
-    content = review.content;
-  }
 </script>
 
 {#if author && content}
-  <div class="nav">
-    <button on:click={prevReview} disabled={reviewsIndex === 0}>Prev</button>
-    <button
-      on:click={nextReview}
-      disabled={reviewsIndex === randomizedReviews.length - 1}>Next</button
-    >
-  </div>
   <blockquote>
-    <p>{author}:</p>
     <cite>{content}</cite>
+    <p>{author}</p>
   </blockquote>
 {/if}
 
 <style>
+  cite {
+    display: block;
+    margin-bottom: 1rem;
+  }
+
   p {
-    margin-bottom: 1rem;
-  }
-
-  .nav {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  .nav button {
-    font-weight: 700;
-    font-size: 10px;
-    padding: 4px 16px;
-    margin-bottom: 1rem;
+    text-transform: capitalize;
+    text-align: end;
   }
 </style>
