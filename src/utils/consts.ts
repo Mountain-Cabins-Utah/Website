2f6905213f787e264b5d74a41224b6766f8fa7f7
Antony Faris <acp325@gmail.com>
2023-06-07 11:46:47 -0500

    hostfully gh workflow

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -2,8 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export function getApiKey() {
-  if (import.meta.env) {
-    return import.meta.env.HOSTFULLY_API_KEY;
+  let apiKey = "";
+  if (import.meta.env && import.meta.env.HOSTFULLY_API_KEY) {
+    apiKey = import.meta.env.HOSTFULLY_API_KEY;
+  } else if (process.env.HOSTFULLY_API_KEY) {
+    apiKey = process.env.HOSTFULLY_API_KEY;
   }
-  return process.env.HOSTFULLY_API_KEY;
+
+  if (apiKey === "") throw new Error("HOSTFULLY_API_KEY is not defined");
+  return apiKey;
 }
